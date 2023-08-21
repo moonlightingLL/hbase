df2ac0c291201fcc4e60199f7a2e42540541e9be
moonlightingLL <nancyliuu@163.com>
2023-08-21 23:05:05 +0800

    adding 0 check

--- hbase-common/src/main/java/org/apache/hadoop/hbase/io/ByteBuffAllocator.java
+++ hbase-common/src/main/java/org/apache/hadoop/hbase/io/ByteBuffAllocator.java
@@ -172,7 +172,7 @@ public class ByteBuffAllocator {
       // released the BBs it used, the handler might have processed one more read req. On an avg 2x
       // we consider and consider that also for the max buffers to pool
       if (poolBufSize == 0) {
-       throw new IllegalArgumentException("BUFFER_SIZE_KEY cannot be zero");
+       throw new IllegalArgumentException("BUFFER_SIZE_KEY cannot be zero.");
       }
       int bufsForTwoMB = (2 * 1024 * 1024) / poolBufSize;
       int maxBuffCount =
